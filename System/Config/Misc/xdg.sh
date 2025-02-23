6fb071ebb5a241beab2306b02ecf2f97b75901ee
Drayux <xenoderg+github@gmail.com>
2025-02-23 17:00:52 -0500

    Gentoo-inspired system scripts

---
+++ System/Config/Misc/xdg.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Desktop spec environment
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_CACHE_HOME="${HOME}/.local/cache"
+export XDG_STATE_HOME="${HOME}/.local/state"
+
+# Runtime directory (/run/user/...) for session management
+if test -z "${XDG_RUNTIME_DIR}"; then
+	rundir="/tmp/run/${UID}"
+	mkdir -p ${rundir} -m 0700
+	export XDG_RUNTIME_DIR="${rundir}"
+fi
