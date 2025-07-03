e4f854909955f6dd2ec0b808ac757e1fa7d0fe44
Drayux <xenoderg+github@gmail.com>
2025-07-03 18:44:25 -0400

    Refactor telescope toggler because I'm difficult

--- Dotfiles/nvim/plugins/telescope.lua
+++ Dotfiles/nvim/plugins/telescope.lua
@@ -6,63 +6,73 @@
 
 -- Options generator for choice telescope pickers that allows the inclusion of
 -- hidden files to be toggled with a keybind - Always starts in hidden mode
+-- NOTE: If inverting with _show, it is recommended to set prompt_title in opts
+-- before calling toggle_find (else the + indicator cannot be appended on init)
 -- > Adapted from https://github.com/nvim-telescope/telescope.nvim/issues/2874#issuecomment-1900967890
-local _toggler = function(picker_fn, _title)
-	local opts; opts = {
-		attach_mappings = function(_, map_fn)
-			local toggle = function(prompt_bufnr)
-				local prompt_text = require("telescope.actions.state").get_current_line()
-				require("telescope.actions").close(prompt_bufnr)
+local toggle_find = function(picker_fn, opts, _show)
+	opts = (type(opts) == "table") and opts or {}
 
-				opts.default_text = prompt_text
-				if opts.hidden then
-					opts.hidden = false
-					opts.no_ignore = false
-					opts.prompt_title = _title
-				else
-					opts.hidden = true
-					opts.no_ignore = true
-					if type(_title) == "string" then
-						opts.prompt_title = _title .. " (+)"
-					end
-				end
-
-				-- This looks like a recursive call, but (I think) it is leak-safe!
-				-- Telescope's :find() call returns immediately, spawning an async
-				-- thread to handle the picker logic, so the hide mapping would spawn
-				-- a new picker instance in the same "async type of vibe" that any
-				-- regular keybind would.
-				-- TLDR: Neovim's event handler is the parent function in the call
-				-- stack that invokes the new picker, not the picker logic itself,
-				-- despite the nested closure ref. (I think.)
-				picker_fn(opts)
+	local title = opts.prompt_title -- or nil (holds original title)
+	local state = require("telescope.actions.state")
+	local toggle = function(prompt_bufnr)
+		if prompt_bufnr then
+			local picker = state.get_current_picker(prompt_bufnr)
+			if not title then
+				title = picker and picker.prompt_title or "<unknown>"
 			end
 
-			map_fn({ "n", "i" }, "<C-h>", toggle)
-			return true
-		end,
+			-- TODO: Try skipping this
+			require("telescope.actions").close(prompt_bufnr)
+		end
 
-		-- Defaults - modified by attach_mappings
-		hidden = false,
-		no_ignore = false,
-		prompt_title = _title
-	}
+		opts.default_text = state.get_current_line()
+		if opts.hidden then
+			opts.hidden = false
+			opts.no_ignore = false
+			opts.prompt_title = title
+		else
+			opts.hidden = true
+			opts.no_ignore = true
+			if type(title) == "string" then
+				opts.prompt_title = title .. " (+)"
+			end
+		end
 
-	picker_fn(opts)
+		-- This looks like a recursive call, but (I think) it is leak-safe!
+		-- Telescope's :find() call returns immediately, spawning an async
+		-- thread to handle the picker logic, so the hide mapping would spawn
+		-- a new picker instance in the same "async type of vibe" that any
+		-- regular keybind would.
+		-- TLDR: Neovim's event handler is the parent function in the call
+		-- stack that invokes the new picker, not the picker logic itself,
+		-- despite the nested closure ref. (I think.)
+		picker_fn(opts)
+	end
+
+	opts.attach_mappings = function(_, map_fn)
+		map_fn({ "n", "i" }, "<C-h>", toggle)
+		return true
+	end
+
+	if _show then
+		-- Start with hidden files visible
+		toggle()
+	else
+		-- Run the picker like normal
+		picker_fn(opts)
+	end
 end
 
 -- Wrapped :Telescope find_files
 local ffplus = function()
 	local picker = require("telescope.builtin").find_files
-	local title = "Find Files"
-	_toggler(picker, title)
+	toggle_find(picker)
 end
 
 -- Wrapped :Telescope live_grep
 local lgplus = function()
 	local picker = require("telescope.builtin").live_grep
-	local title = "Live Grep"
-	_toggler(picker, title)
+	toggle_find(picker)
 end
 
 local spec = { 
