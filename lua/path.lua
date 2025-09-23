a401b3415feab40e81c4ac7e4791e4d830150d60
Drayux <xenoderg+github@gmail.com>
2025-09-23 03:33:54 -0400

    Path split/build done (probably idk I haven't tested it)

--- lua/path.lua
+++ lua/path.lua
@@ -14,7 +14,31 @@
 
 local api = {}
 
-local function _split(path_str, varpath_tbl)
+-- Creates and returns a path obj table only (no MT)
+-- Copies data from ref, if provided
+local function _new(path_ref)
+	local obj = {
+		_data = {}, 
+		_vars = varpath_tbl,
+		_index = nil, -- Placeholder
+		_pref = 0, -- Count of leading parent refs (i.e. ../../../dir -> 3)
+
+		absolute = false, -- TODO
+	}
+
+	if path_ref and (type(path_ref._data) == "table") then
+		for _, val in ipairs(path_ref._data) do
+			table.insert(obj._data, val)
+		end
+		_data._vars = path_ref._vars
+		_data._pref = path_ref._pref
+		_data.absolute = path_ref.absolute
+	end
+
+	return path_ref
+end
+
+local function _split(path_str, varpath_tbl, append_mode)
 	-- path_str already validated; don't validate varpath_tbl until first lookup
 
 	local splits = {}
@@ -33,7 +57,7 @@ local function _split(path_str, varpath_tbl)
 			if varpath_key then
 				local varpath = varpath_tbl[varpath_key]
 				assert(varpath, string.format("No such path variable `$%s`", varpath_key))
-				-- current_varpath = varpath_key
+				-- current_varpath = varpath_key -- Not used at path start
 
 				splitter(segment, true, depth + 1) -- Recurse on path variables
 			else
@@ -66,12 +90,63 @@ local function _split(path_str, varpath_tbl)
 		end
 	end
 
-	splitter(path, true, 1)
+	splitter(path, not append_mode, 1)
 	splits._absolute = absolute
-	return splits
+	return splits --[[, err]]
 end
 
-local function _build(path_str, varpath_tbl)
+-- Build creates a new table always
+-- path_obj is a reference to a path to copy or nil
+local function _build(path_obj, path_splits)
+	local _copy = _new(path_obj)
+	local _warning = false -- For helpful output (only)
+
+	if not path_splits then
+		-- Return right away if nothing to append (cleanup for path:Append())
+		return _copy
+	end
+
+	local data = _copy._data -- Ref
+	local accept_index = #data -- TODO: test this
+	for _, split in ipairs(path_splits) do
+		if split == "." then
+			-- Ignore
+		elseif split == ".." then
+			if accept_index > 0 then
+				-- Step backward up the filetree
+				-- TODO: This differs slightly from unix: where if a given path
+				-- is a symlink, then traversing back up will navigate into the
+				-- directory containing the target of the symlink instead. That
+				-- functionality is not reflected here. Should it be?
+
+				data[accept_index] = nil
+				accept_index = accept_index - 1
+
+				if not _warning then
+					print("Warning: it is advisable to avoid ../ within spec install paths")
+					_warning = true
+				end
+			elseif not path_splits.absolute then
+				-- Index is 0: no known dirs to step up
+				_copy._pref = _copy._pref + 1
+			else
+				-- Path is: /(../)whatever
+				-- (this is technically okay: in unix /../ -> /; but high chance of user error in specs)
+				assert(false, "Unable to specify parent of root directory")
+			end
+		else
+			accept_index = accept_index + 1
+			splits[accept_index] = segment
+		end
+	end
+
+	return setmetatable(_copy, {
+		__index = api,
+		__newindex = function()
+			-- Not rigorous here, just help track of developer mistakes
+			error("Attempt to add new member to path table")
+		end
+	})
 end
 
 --
@@ -82,11 +157,14 @@ end
 
 function api.GetAbsolute(self)
 	-- TODO: Get (guess) an absolute path or return if already absolute
+	-- TODO: Should this also resolve all parent path refs? (../)
 end
 
-function api.Append(self, subdir)
-	-- TODO: Add to the current path with a new subdirectory
-	-- (consider option to copy and add to a new path instance instead?)
+-- Copies and returns a modified path
+function api.Append(self, subpath_str)
+	local split_data = subpath_str and _split(subpath_str, self.vars, true)
+		or nil
+	return _build(self, split_data)
 end
 
 function api.Search(self)
@@ -102,24 +180,11 @@ return setmetatable({}, {
 		assert(#path_str > 0, "Path cannot be an empty string")
 		
 		-- Paths are stored as arrays of splits
-		local path_data, err = _split(path_str, varpath_tbl)
-		err = err or _build(splits) -- Skip build step if error in split step
+		local split_data, err = _split(path_str, varpath_tbl, false)
 		if err then
 			error(err)
 		end
 
-		return path_data and setmetatable({
-			_data = path_data, 
-			_vars = varpath_tbl,
-			_index = nil, -- Placeholder
-
-			absolute = false, -- TODO
-		}, {
-			__index = api,
-			__newindex = function()
-				-- Not rigorous here, just help track of developer mistakes
-				error("Attempt to add new member to path table")
-			end
-		})
+		return _build(nil, split_data)
 	end
 })
